af576c06b0d5d2407fdb26d0a0f839de2e8ee909
akrobi <gwangui00000@gmail.com>
2022-09-15 22:05:40 +0300

    setup flask env

--- scripts/app.py
+++ scripts/app.py
@@ -1,1 +1,17 @@
 import os
+
+from flask import Flask, request, jsonify, render_template
+
+# create an instance of the Flask class
+app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     """Render the index page."""
+#     return jsonify({"status": "success"})
+
+# @app.route('/api/generate')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', debug=True, port=port)
